fea1e92f34278febb096f93bb157e763057b2e4a
nilsegger <nilsxegger@gmail.com>
2024-10-31 14:54:43 +0100

    explosive simulation

--- advection-reflection/main.cpp
+++ advection-reflection/main.cpp
@@ -4,12 +4,14 @@
 #include <iostream>
 #include <vector>
 #include <Eigen/Core>
+#include <chrono>
 
-const int RES_X = 128;
-const int RES_Y = 128;
-const int RES_Z = 128;
+const int RES_X = 64;
+const int RES_Y = 64;
+const int RES_Z = 64;
 const int WIDTH = 768;
 const int HEIGHT = 768;
+
 
 // Vertex shader source code
 const char *vertexShaderSource = R"(
@@ -64,7 +66,10 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
 
 class FluidSim {
 public:
-    FluidSim(): m_density(RES_X * RES_Y * RES_Z, 0.0), m_velocity(RES_X * RES_Y * RES_Z, Eigen::Vector3f::Zero()) {
+
+    FluidSim(): m_vu(RES_X * RES_Y * RES_Z, 0.0f), m_vv(RES_X * RES_Y * RES_Z, 0.0f),
+                m_vw(RES_X * RES_Y * RES_Z, 0.0f), m_s(RES_X*RES_Y*RES_Z, 1.0f),
+                m_density(RES_X * RES_Y * RES_Z, 0.0) {
     }
 
     void create_sphere_density() {
@@ -76,28 +81,11 @@ public:
                     int xx = x - RES_X / 2;
                     int yy = y - RES_Y / 2;
                     int zz = z - RES_Z / 2;
-                    if (xx * xx + yy * yy + zz * zz < 32 * 32) {
-                        m_density[index] = float(32 * 32 - (xx * xx + yy * yy + zz * zz)) / float(32 * 32) * 0.4;
+                    if (xx * xx + yy * yy + zz * zz < 16 * 16) {
+                        m_density[index] = float(16 * 16 - (xx * xx + yy * yy + zz * zz)) / float(16 * 16) * 0.1;
                     } else {
                         m_density[index] = 0.0;
                     }
-                }
-            }
-        }
-    }
-
-    void create_circular_flow() {
-        Eigen::Vector3f center(RES_X / 2.0f, RES_Y / 2.0f, RES_Z / 2.0f); // Center of vortex
-        float strength = 1.0f; // Controls the intensity of the rotation
-
-        for (int x = 0; x < RES_X; x++) {
-            for (int y = 0; y < RES_Y; y++) {
-                for (int z = 0; z < RES_Z; z++) {
-                    Eigen::Vector3f pos(x, y, z);
-                    Eigen::Vector3f direction = pos - center;
-                    direction = Eigen::Vector3f(-direction.y(), direction.x(), 0.0f); // Rotate around z-axis
-                    direction.normalize();
-                    m_velocity[index(x, y, z)] = direction * strength / (pos - center).norm(); // Decay with distance
                 }
             }
         }
@@ -127,114 +115,197 @@ public:
         glUniform1i(glGetUniformLocation(shaderProgram, "densityTexture"), 0);
     }
 
-    void update() {
-
-        float m_step = 0.01f;
+    void update(float t_step) {
 
         // Great video https://www.youtube.com/watch?v=iKAVRgIrUOU
         // 1. Modify velocity values (e.g. gravity)
-        // 2. make fluid incompressable (projection)
-        // 3. Move the velocity field (advection)
 
-        mac_cormack_advection(m_step / 2.0f);
-        projection();
-        reflection();
-        mac_cormack_advection(m_step / 2.0f);
-        projection();
+        iterate([this, t_step](int x, int y, int z) {
+            m_vv[index(x, y, z)] = m_vv[index(x, y, z)] + (t_step * -9.81f);
+        });
+
+        std::vector ss_vv(m_vv);
+        std::vector ss_vu(m_vu);
+        std::vector ss_vw(m_vw);
+        std::vector ss_d(m_density);
+
+        // 2. make fluid incompressable (projection)
+        iterate([this, t_step, &ss_vv, &ss_vu, &ss_vw](int x, int y, int z) {
+
+            int i = index(x, y, z);
+            int ixpp = index(x+1, y, z);
+            int ixmm = index(x-1, y, z);
+            int iypp = index(x, y+1, z);
+            int iymm = index(x, y-1, z);
+            int izpp = index(x, y, z+1);
+            int izmm = index(x, y, z-1);
+
+            // Gauss-Seidel method
+            float divergence = 1.0f * (m_vu[i] - m_vu[ixpp] + m_vv[i] - m_vv[iypp] + m_vw[i] - m_vw[izpp]);
+            float s = m_s[ixpp] + m_s[ixmm] + m_s[iypp]+ m_s[iymm]+ m_s[izpp] + m_s[izmm];
+            ss_vu[i] += divergence * (m_s[i]) / s;
+            ss_vu[ixpp] -= divergence * (m_s[ixpp]) / s;
+            ss_vv[i] += divergence * (m_s[i]) / s;
+            ss_vv[iypp] -= divergence * (m_s[iypp]) / s;
+            ss_vw[i] += divergence * (m_s[i]) / s;
+            ss_vw[izpp] -= divergence * (m_s[izpp]) / s;
+        });
+
+        // 3. Move the velocity field (advection)
+        iterate([this, t_step, &ss_vv, &ss_vu, &ss_vw, &ss_d](int x, int y, int z) {
+            // semi-lagrangian
+
+            // TODO: check for obstacles
+            int i = index(x, y, z);
+
+            // Backtrack, find out where "particles" come from
+            // u component
+            m_vu[i] = sample(ss_vu, ss_vv, ss_vw, x, y, z, SampleDirection::X, t_step);
+            m_vv[i] = sample(ss_vu, ss_vv, ss_vw, x, y, z, SampleDirection::Y, t_step);
+            m_vw[i] = sample(ss_vu, ss_vv, ss_vw, x, y, z, SampleDirection::Z, t_step);
+
+            m_density[i] = sample_density(ss_d, ss_vu, ss_vv, ss_vw, x, y, z, t_step);
+
+        });
     }
 
 private:
-
     // Should be represeneted as a staggered grid. velocity vectors are not in the center of the cell
     // https://www.researchgate.net/profile/Ke-Qing-Xia/publication/325053508/figure/fig1/AS:625094766370816@1526045614156/Schematic-of-the-staggered-grid_W640.jpg
-    std::vector<Eigen::Vector3f> m_velocity;
+    std::vector<float> m_vu;
+    std::vector<float> m_vv;
+    std::vector<float> m_vw;
+
+    // 1 means fluid cell, 0 means obstable
+    std::vector<float> m_s;
+
     std::vector<float> m_density;
 
     float m_advection_speed = 1.0f;
 
-    static size_t index(int x, int y, int z) {
-        return std::min(std::max(0, x + y * RES_X + z * RES_X * RES_Y), RES_X*RES_Y*RES_Z - 1);
+    // staggered grid index
+    static int index(int x, int y, int z) {
+        return std::min(std::max(0, x + y * (RES_X) + z * (RES_X) * (RES_Y)),
+                        (RES_X) * (RES_Y) * (RES_Z) - 1);
     }
 
-    void mac_cormack_advection(float t_step) {
-
-        // create backup
-        std::vector<float> d(m_density);
-        std::vector<Eigen::Vector3f> v(m_velocity);
-
-        // predictor step
-        for(int x = 1; x < RES_X - 1; x++) {
-            for(int y = 1; y < RES_Y - 1; y++) {
-                for(int z = 1; z < RES_Z - 1; z++) {
-                    size_t i = index(x, y, z);
-                    Eigen::Vector3f dx = v[index(x + 1, y, z)] - v[i]; // Spacing is one?
-                    Eigen::Vector3f dy = v[index(x, y + 1, z)] - v[i]; // Spacing is one?
-                    Eigen::Vector3f dz = v[index(x, y, z + 1)] - v[i]; // Spacing is one?
-                    m_velocity[i] = v[i] - t_step * (dx + dy + dz);
+    template<typename F>
+    void iterate(const F &func) {
+        for (int x = 1; x < RES_X - 1; x++) {
+            for (int y = 1; y < RES_Y - 1; y++) {
+                for (int z = 1; z < RES_Z - 1; z++) {
+                    func(x, y, z);
                 }
             }
         }
+    }
 
-        // Corrector Step
-        for(int x = 1; x < RES_X - 1; x++) {
-            for(int y = 1; y < RES_Y - 1; y++) {
-                for(int z = 1; z < RES_Z - 1; z++) {
-                    size_t i = index(x, y, z);
-                    Eigen::Vector3f dx = m_velocity[i] - m_velocity[index(x - 1, y, z)]; // Spacing is one?
-                    Eigen::Vector3f dy = m_velocity[i] - m_velocity[index(x, y - 1, z)]; // Spacing is one?
-                    Eigen::Vector3f dz = m_velocity[i] - m_velocity[index(x, y, z - 1)]; // Spacing is one?
-                    m_velocity[i] = (m_velocity[i] + v[i]) / 2.0f - t_step / 2.0f * (dx + dy + dz);
-                }
+    enum SampleDirection {
+        X, Y, Z
+    };
+
+    // Move back by new velocity and uniformly sample value
+    float sample(std::vector<float>& ss_vu, std::vector<float>& ss_vv, std::vector<float>& ss_vw, int x, int y, int z, SampleDirection direction, float t_step) {
+        int i = index(x, y, z);
+
+        // Vector to move back by, different depending on sample direction
+        float vx = ss_vu[i] * t_step;
+        float vy = ss_vv[i] * t_step;
+        float vz = ss_vw[i] * t_step;
+
+        switch(direction) {
+            case X: {
+                vy = (ss_vv[i] + ss_vv[index(x+1, y, z)] + ss_vv[index(x, y+1, z)] + ss_vv[index(x+1, y+1, z)]) / 4.0f;
+                vz = (ss_vw[i] + ss_vw[index(x+1, y, z)] + ss_vw[index(x, y, z+1)] + ss_vw[index(x+1, y, z+1)]) / 4.0f;
+                break;
+            }
+            case Y: {
+                vx = (ss_vu[i] + ss_vu[index(x+1, y, z)] + ss_vu[index(x, y+1, z)] + ss_vu[index(x+1, y+1, z)]) / 4.0f;
+                vz = (ss_vw[i] + ss_vw[index(x, y+1, z)] + ss_vw[index(x, y, z+1)] + ss_vw[index(x, y+1, z+1)]) / 4.0f;
+                break;
+            }
+            case Z: {
+                vx = (ss_vu[i] + ss_vu[index(x+1, y, z)] + ss_vu[index(x, y, z+1)] + ss_vu[index(x+1, y, z+1)]) / 4.0f;
+                vy = (ss_vw[i] + ss_vw[index(x, y+1, z)] + ss_vw[index(x, y, z+1)] + ss_vw[index(x, y+1, z+1)]) / 4.0f;
+                break;
             }
         }
 
-        // Clamping
-        for(int x = 1; x < RES_X - 1; x++) {
-            for(int y = 1; y < RES_Y - 1; y++) {
-                for(int z = 1; z < RES_Z - 1; z++) {
-                    size_t i = index(x, y, z);
+        // Move back
+        float nux = (float)x - vx;
+        float nvx = (float)y - vy;
+        float nwx = (float)z - vz;
 
-                    Eigen::Vector3f min_val = v[i];
-                    Eigen::Vector3f max_val = v[i];
-                    for (int dx = -1; dx <= 1; dx++) {
-                        for (int dy = -1; dy <= 1; dy++) {
-                            for (int dz = -1; dz <= 1; dz++) {
-                                size_t neighbor_index = index(x + dx, y + dy, z + dz);
-                                min_val = min_val.cwiseMin(v[neighbor_index]);
-                                max_val = max_val.cwiseMax(v[neighbor_index]);
-                            }
-                        }
-                    }
-
-                    m_velocity[i] = m_velocity[i].cwiseMax(min_val).cwiseMin(max_val);
-                }
+        switch (direction) {
+            case X: {
+                float w0 = nux - std::floor(nux);
+                float w1 = 1.0f - w0;
+                return w0 * ss_vu[index(nux, nvx, nwx)] + w1 * ss_vu[index(nux + 1, nvx, nwx)];
+            }
+            case Y: {
+                float w0 = nvx - std::floor(nvx);
+                float w1 = 1.0f - w0;
+                return w0 * ss_vu[index(nux, nvx, nwx)] + w1 * ss_vu[index(nux, nvx+1, nwx)];
+            }
+            case Z: {
+                float w0 = nwx - std::floor(nwx);
+                float w1 = 1.0f - w0;
+                return w0 * ss_vu[index(nux, nvx, nwx)] + w1 * ss_vu[index(nux, nvx, nwx+1)];
             }
         }
 
-
-        for(int x = 1; x < RES_X - 1; x++) {
-            for(int y = 1; y < RES_Y - 1; y++) {
-                for(int z = 1; z < RES_Z - 1; z++) {
-                    Eigen::Vector3f vel = m_velocity[index(x, y, z)];
-                    m_density[index(x, y, z)] = d[index(x - vel.x(), y - vel.y(), z - vel.z())];
-                }
-            }
-        }
-
-
+        return 0.0f;
     }
 
-    void projection() {
+        // Move back by new velocity and uniformly sample value
+    float sample_density(std::vector<float>& density, std::vector<float>& ss_vu, std::vector<float>& ss_vv, std::vector<float>& ss_vw, int x, int y, int z, float t_step) {
+        int i = index(x, y, z);
 
+        // Vector to move back by, different depending on sample direction
+        float vx = (ss_vu[i] + ss_vu[index(x+1, y, z)]) / 2.0f * t_step;
+        float vy = (ss_vv[i] + ss_vv[index(x, y+1, z)]) / 2.0f * t_step;
+        float vz = (ss_vw[i] + ss_vw[index(x, y, z+1)]) / 2.0f * t_step;
+
+        // Move back, lands within 4 cells origins -> interpolate
+        float nx = static_cast<float>(x) - vx;
+        float ny = static_cast<float>(y) - vy;
+        float nz = static_cast<float>(z) - vz;
+
+        float x1 = std::floor(nx);
+        float x2 = nx - x1 >= 0.5 ? std::ceil(nx) : x1 - 1;
+        float y1 = std::floor(ny);
+        float y2 = ny - y1 >= 0.5 ? std::ceil(ny) : y1 - 1;
+        float z1 = std::floor(nz);
+        float z2 = ny - z1 >= 0.5 ? std::ceil(nz) : z1 - 1;
+
+        if(x1 > x2) std::swap(x1, x2);
+        if(y1 > y2) std::swap(y1, y2);
+        if(z1 > z2) std::swap(z1, z2);
+        if(x1 == x2) x2++;
+        if(y1 == y2) y2++;
+        if(z1 == z2) z2++;
+
+        // Trilinier interpolation
+        float u = (nx - x1) / (x2 - x1);
+        float v = (ny - y1) / (y2 - y1);
+        float w = (nz - z1) / (z2 - z1);
+
+        int i1 = index(x1, y1, z1); float w1 = (1.0f - u) * (1.0f - v) * (1.0f - w);
+        int i2 = index(x2, y1, z1); float w2 = u * (1.0f - v) * (1.0f - w);
+        int i3 = index(x1, y2, z1); float w3 = (1.0f - u) * v * (1.0f - w);
+        int i4 = index(x1, y1, z2); float w4 = (1.0f - u) * (1.0f - v) * w;
+        int i5 = index(x2, y2, z1); float w5 = u * v * (1.0f - w);
+        int i6 = index(x2, y1, z2); float w6 = u * (1.0f - v) * w;
+        int i7 = index(x1, y2, z2); float w7 = (1.0f - u) * v * w;
+        int i8 = index(x2, y2, z2); float w8 = u*v*w;
+
+        return w1 * density[i1] + w2 * density[i2] + w3 * density[i3] + w4 * density[i4] + w5 * density[i5] + w6 * density[i6] + w7 * density[i7] + w8 * density[i8];
     }
-
-    void reflection() {
-
-    }
-
 };
 
 int main() {
+    std::ios_base::sync_with_stdio(false);
+
     // Initialize GLFW
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
@@ -309,15 +380,18 @@ int main() {
 
     FluidSim sim;
     sim.create_sphere_density();
-    sim.create_circular_flow();
+
+    double deltaTime = 0.0;
 
     // Render loop
     while (!glfwWindowShouldClose(window)) {
+        auto startTime = std::chrono::high_resolution_clock::now();
         // Input
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(window, true);
 
-        sim.update();
+
+        sim.update(std::min(1.0 / 30.0f, deltaTime / 1000.0f));
 
         // Render
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
@@ -334,6 +408,13 @@ int main() {
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        auto endTime = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double, std::milli> elapsed = endTime - startTime;
+        deltaTime = elapsed.count();
+
+        // Print or log the frame time in milliseconds
+        std::cout << "Frame time: " << deltaTime << " ms. Using " << std::min(1.0 / 30.0f, deltaTime / 1000.0f) << "s" << std::endl;
 
         // Swap buffers and poll events
         glfwSwapBuffers(window);
