9d95273c7cd2727d19c8d535c99aa1dcca49b7a6
nilsegger <nilsxegger@gmail.com>
2024-11-04 10:29:31 +0100

    basic gravity version

--- advection-reflection/main.cpp
+++ advection-reflection/main.cpp
@@ -8,12 +8,9 @@
 #include <chrono>
 
 
-const int RES_X = 64;
-const int RES_Y = 64;
-const int RES_Z = 64;
-const float HW = 0.1; // VOXEL WIDTH
-const float HH = 0.1; // VOXEL Height
-const float HD = 0.1; // VOXEL Depth
+const int RES_X = 32;
+const int RES_Y = 32;
+const int RES_Z = 32;
 
 const int WIDTH = 768;
 const int HEIGHT = 768;
@@ -79,28 +76,28 @@ class Line {
     Eigen::Vector3f endPoint;
     Eigen::Matrix4f MVP;
     Eigen::Vector3f lineColor;
+
 public:
     Line(Eigen::Vector3f start, Eigen::Vector3f end) {
-
         startPoint = start;
         endPoint = end;
         lineColor = Eigen::Vector3f(0.0f, 0.0f, 1.0f); // vec3(1,1,1);
         //MVP = mat4(1.0f);
 
         const char *vertexShaderSource = "#version 330 core\n"
-            "layout (location = 0) in vec3 aPos;\n"
-            "uniform mat4 MVP;\n"
-            "void main()\n"
-            "{\n"
-            "   gl_Position = MVP * vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-            "}\0";
+                "layout (location = 0) in vec3 aPos;\n"
+                "uniform mat4 MVP;\n"
+                "void main()\n"
+                "{\n"
+                "   gl_Position = MVP * vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                "}\0";
         const char *fragmentShaderSource = "#version 330 core\n"
-            "out vec4 FragColor;\n"
-            "uniform vec3 color;\n"
-            "void main()\n"
-            "{\n"
-            "   FragColor = vec4(color, 1.0f);\n"
-            "}\n\0";
+                "out vec4 FragColor;\n"
+                "uniform vec3 color;\n"
+                "void main()\n"
+                "{\n"
+                "   FragColor = vec4(color, 1.0f);\n"
+                "}\n\0";
 
         // vertex shader
         int vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -125,8 +122,8 @@ public:
         glDeleteShader(fragmentShader);
 
         vertices = {
-             start.x(), start.y(), start.z(),
-             end.x(), end.y(), end.z(),
+            start.x(), start.y(), start.z(),
+            end.x(), end.y(), end.z(),
         };
 
         glGenVertexArrays(1, &VAO);
@@ -136,12 +133,11 @@ public:
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices.data(), GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
         glEnableVertexAttribArray(0);
 
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
-
     }
 
     void draw(Eigen::Matrix4Xf mvp, Eigen::Vector3f color) {
@@ -155,9 +151,9 @@ public:
 
     ~Line() {
         // TODO: this leads to seg fault?
-        glDeleteVertexArrays(1, &VAO);
-        glDeleteBuffers(1, &VBO);
-        glDeleteProgram(shaderProgram);
+        // glDeleteVertexArrays(1, &VAO);
+        // glDeleteBuffers(1, &VBO);
+        // glDeleteProgram(shaderProgram);
     }
 };
 
@@ -166,11 +162,10 @@ public:
     FluidSim(): m_vu(RES_X * RES_Y * RES_Z, 0.0f), m_vv(RES_X * RES_Y * RES_Z, 0.0f),
                 m_vw(RES_X * RES_Y * RES_Z, 0.0f), m_s(RES_X * RES_Y * RES_Z, 1.0f),
                 m_density(RES_X * RES_Y * RES_Z, 0.0) {
-
-        for(int i = 0; i < RES_X*RES_Y*RES_Z; i++) {
-            int x,y,z;
-            inverseIndex(i, x,y,z);
-            if(x == 0 || y == 0 || z == 0) {
+        for (int i = 0; i < RES_X * RES_Y * RES_Z; i++) {
+            int x, y, z;
+            inverseIndex(i, x, y, z);
+            if (x == 0 || y == 0 || z == 0) {
                 m_s[i] = 0.0f;
             }
         }
@@ -178,7 +173,6 @@ public:
         iterate([this](const int i, const int x, const int y, const int z) {
             m_vv[i] = 0.0f;
         });
-
     }
 
     void create_sphere_density() {
@@ -243,14 +237,16 @@ public:
 
             // Gauss-Seidel method
             float divergence = m_vu[i] - safeSample(m_vu, ixpp, m_vu[i]) +
-                                m_vv[i] - safeSample(m_vv, iypp, m_vv[i]) +
-                                m_vw[i] - safeSample(m_vw, izpp, m_vw[i]);
+                               m_vv[i] - safeSample(m_vv, iypp, m_vv[i]) +
+                               m_vw[i] - safeSample(m_vw, izpp, m_vw[i]);
 
             float s = m_s[ixpp] + m_s[ixmm] + m_s[iypp] + m_s[iymm] + m_s[izpp] + m_s[izmm];
 
             if (divergence == 0) {
                 return;
             }
+
+            std::cerr << divergence << '\n';
 
             float p = divergence / s;
 
@@ -278,9 +274,8 @@ public:
         });
     }
 
-    void draw_velocity_field(Line& l, Line& l1, Line& l2) {
+    void draw_velocity_field(Line &l, Line &l1, Line &l2) {
         iterate([this, &l, &l1, &l2](const int i, const int x, const int y, const int z) {
-
             if (z != 1) return;
 
             float max_w = 0.8f / RES_X;
@@ -290,8 +285,8 @@ public:
             float uv_y = (float(y) / float(RES_Y) - 0.5f) * 2.0f;
             float uv_z = (float(z) / float(RES_Z)) * 2.0f;
 
-
-            Eigen::Transform<float, 3, Eigen::Affine> transform_x = Eigen::Transform<float, 3, Eigen::Affine>::Identity();
+            Eigen::Transform<float, 3, Eigen::Affine> transform_x = Eigen::Transform<float, 3,
+                Eigen::Affine>::Identity();
             float sx = std::copysign(std::min(std::abs(m_vu[i] * max_w), max_w), m_vu[i]);
             transform_x.translate(Eigen::Vector3f(uv_x, uv_y + (.5f / RES_Y), uv_z));
             transform_x.scale(sx);
@@ -299,25 +294,20 @@ public:
             l1.draw(transform_x.matrix(), Eigen::Vector3f(1.0f, 0.0f, 0.0f));
             l2.draw(transform_x.matrix(), Eigen::Vector3f(1.0f, 0.0f, 0.0f));
 
-            Eigen::Transform<float, 3, Eigen::Affine> transform_y = Eigen::Transform<float, 3, Eigen::Affine>::Identity();
+            Eigen::Transform<float, 3, Eigen::Affine> transform_y = Eigen::Transform<float, 3,
+                Eigen::Affine>::Identity();
             float sy = std::copysign(std::min(std::abs(m_vv[i] * max_h), max_h), m_vv[i]);
             transform_y.translate(Eigen::Vector3f(uv_x + (.5f / RES_X), uv_y, uv_z));
             transform_y.scale(sy);
-            float angle = 90.0f * M_PI / 180.0f;  // Convert degrees to radians
+            float angle = 90.0f * M_PI / 180.0f; // Convert degrees to radians
             transform_y.rotate(Eigen::AngleAxisf(angle, Eigen::Vector3f::UnitZ()));
             l.draw(transform_y.matrix(), Eigen::Vector3f(0.0f, 1.0f, 0.0f));
             l1.draw(transform_y.matrix(), Eigen::Vector3f(0.0f, 1.0f, 0.0f));
             l2.draw(transform_y.matrix(), Eigen::Vector3f(0.0f, 1.0f, 0.0f));
         });
-
     }
 
 private:
-
-    struct Voxel {
-        float x_in, y_in, z_in;
-    };
-
     // Should be represeneted as a staggered grid. velocity vectors are not in the center of the cell
     // https://www.researchgate.net/profile/Ke-Qing-Xia/publication/325053508/figure/fig1/AS:625094766370816@1526045614156/Schematic-of-the-staggered-grid_W640.jpg
     std::vector<float> m_vu;
@@ -352,7 +342,7 @@ private:
         for (int i = 0; i < RES_X * RES_Y * RES_Z; i++) {
             int x, y, z;
             inverseIndex(i, x, y, z);
-            if(x != 0 && x < RES_X - 1 && y != 0 && y < RES_Y - 1 && z != 0 && z < RES_Z - 1)
+            if (x != 0 && x < RES_X - 1 && y != 0 && y < RES_Y - 1 && z != 0 && z < RES_Z - 1)
                 func(i, x, y, z);
         }
     }
@@ -362,13 +352,9 @@ private:
     };
 
     // Move back by new velocity and uniformly sample value
-    float sample(std::vector<float> &ss_vu, std::vector<float> &ss_vv, std::vector<float> &ss_vw, const int i, const int x, const int y, const int z,
+    float sample(std::vector<float> &ss_vu, std::vector<float> &ss_vv, std::vector<float> &ss_vw, const int i,
+                 const int x, const int y, const int z,
                  SampleDirection direction, float t_step) {
-
-        if(x == 0 || y == 0 || z == 0 || x >= RES_X - 1 || y >= RES_Y - 1 || z >= RES_Z - 1) {
-            std::cerr << "Invalid xyz.\n";
-            return 0.0f;
-        }
 
         // Vector to move back by, different depending on sample direction
         float vx = safeSample(ss_vu, i, 0.0f) * t_step;
@@ -377,32 +363,38 @@ private:
 
         switch (direction) {
             case X: {
-                vy = (safeSample(ss_vv, i, 0.0f) + ss_vv[index(x - 1, y, z)] + ss_vv[index(x, y + 1, z)] + ss_vv[index(x - 1, y + 1, z)])
+                vy = (safeSample(ss_vv, i, 0.0f) + safeSample(ss_vv, x - 1, y, z, 0.0f) +
+                      safeSample(ss_vv, x, y + 1, z, 0.0f) + safeSample(ss_vv, x - 1, y + 1, z, 0.0f))
                      / 4.0f;
-                vz = (ss_vw[i] + ss_vw[index(x - 1, y, z)] + ss_vw[index(x, y, z + 1)] + ss_vw[index(x - 1, y, z + 1)])
+                vz = (safeSample(ss_vw, i, 0.0f) + safeSample(ss_vw, x - 1, y, z, 0.0f) +
+                      safeSample(ss_vw, x, y, z + 1, 0.0f) + safeSample(ss_vw, x - 1, y, z + 1, 0.0f))
                      / 4.0f;
                 break;
             }
             case Y: {
-                vx = (safeSample(ss_vu, i, 0.0f) + safeSample(ss_vu, x + 1, y, z, 0.0f) + safeSample(ss_vu, x, y-1, z, 0.0f) + safeSample(ss_vu, x + 1, y-1, z, 0.0f))
+                vx = (safeSample(ss_vu, i, 0.0f) + safeSample(ss_vu, x + 1, y, z, 0.0f) +
+                      safeSample(ss_vu, x, y - 1, z, 0.0f) + safeSample(ss_vu, x + 1, y - 1, z, 0.0f))
                      / 4.0f;
-                vz = (safeSample(ss_vw, i, 0.0f) + safeSample(ss_vw, x, y, z+1, 0.0f) + safeSample(ss_vw, x, y-1, z, 0.0f) + safeSample(ss_vw, x, y-1, z+1, 0.0f))
+                vz = (safeSample(ss_vw, i, 0.0f) + safeSample(ss_vw, x, y, z + 1, 0.0f) +
+                      safeSample(ss_vw, x, y - 1, z, 0.0f) + safeSample(ss_vw, x, y - 1, z + 1, 0.0f))
                      / 4.0f;
                 break;
             }
             case Z: {
-                vx = (ss_vu[i] + ss_vu[index(x + 1, y, z)] + ss_vu[index(x, y, z - 1)] + ss_vu[index(x + 1, y, z - 1)])
+                vx = (safeSample(ss_vu, i, 0.0f) + safeSample(ss_vu, x + 1, y, z, 0.0f) +
+                      safeSample(ss_vu, x, y, z - 1, 0.0f) + safeSample(ss_vu, x + 1, y, z - 1, 0.0f))
                      / 4.0f;
-                vy = (ss_vv[i] + ss_vv[index(x, y + 1, z)] + ss_vv[index(x, y, z - 1)] + ss_vv[index(x, y + 1, z - 1)])
+                vy = (safeSample(ss_vv, i, 0.0f) + safeSample(ss_vv, x, y, z - 1, 0.0f) +
+                      safeSample(ss_vv, x, y + 1, z, 0.0f) + safeSample(ss_vv, x, y + 1, z - 1, 0.0f))
                      / 4.0f;
                 break;
             }
         }
 
         // Move back
-        float nu = static_cast<float>(x) - vx;
-        float nv = static_cast<float>(x)- vy;
-        float nw = static_cast<float>(x) - vz;
+        float nu = static_cast<float>(x) + vx;
+        float nv = static_cast<float>(x) + vy;
+        float nw = static_cast<float>(x) + vz;
 
         switch (direction) {
             case X: return interpolate(nu, nv, nw, ss_vu);
@@ -423,31 +415,25 @@ private:
         float vy = (ss_vv[i] + ss_vv[index(x, y + 1, z)]) / 2.0f * t_step;
         float vz = (ss_vw[i] + ss_vw[index(x, y, z + 1)]) / 2.0f * t_step;
 
+
         // Move back, lands within 4 cells origins -> interpolate
         float nx = static_cast<float>(x) - vx;
         float ny = static_cast<float>(y) - vy;
         float nz = static_cast<float>(z) - vz;
 
-        return interpolate(nx, ny, nz, m_density);
+        return interpolate(nx, ny, nz, density);
     }
 
 
-    float interpolate(const float nx, const float ny, const float nz, const std::vector<float>& samples) {
+    float interpolate(const float nx, const float ny, const float nz, const std::vector<float> &samples) {
+
         float x1 = std::floor(nx);
-        float x2 = nx - x1 >= 0.5 ? std::ceil(nx) : x1 - 1;
+        float x2 = x1 + 1;
         float y1 = std::floor(ny);
-        float y2 = ny - y1 >= 0.5 ? std::ceil(ny) : y1 - 1;
+        float y2 = y1 + 1;
         float z1 = std::floor(nz);
-        float z2 = ny - z1 >= 0.5 ? std::ceil(nz) : z1 - 1;
+        float z2 = z1 + 1;
 
-        if (x1 > x2) std::swap(x1, x2);
-        if (y1 > y2) std::swap(y1, y2);
-        if (z1 > z2) std::swap(z1, z2);
-        if (x1 == x2) x2++;
-        if (y1 == y2) y2++;
-        if (z1 == z2) z2++;
-
-        // Trilinier interpolation
         float u = (nx - x1) / (x2 - x1);
         float v = (ny - y1) / (y2 - y1);
         float w = (nz - z1) / (z2 - z1);
@@ -470,44 +456,39 @@ private:
         float w8 = u * v * w;
 
         float result = w1 * safeSample(samples, i1, 0.0f) +
-                        w2 * safeSample(samples, i2, 0.0f) +
-                        w3 * safeSample(samples, i3, 0.0f) +
-                        w4 * safeSample(samples, i4, 0.0f) +
-                        w5 * safeSample(samples, i5, 0.0f) +
-                        w6 * safeSample(samples, i6, 0.0f) +
-                        w7 * safeSample(samples, i7, 0.0f) +
-                        w8 * safeSample(samples, i8, 0.0f);
-
-        if(result > 0) {
-            return result;
-        }
+                       w2 * safeSample(samples, i2, 0.0f) +
+                       w3 * safeSample(samples, i3, 0.0f) +
+                       w4 * safeSample(samples, i4, 0.0f) +
+                       w5 * safeSample(samples, i5, 0.0f) +
+                       w6 * safeSample(samples, i6, 0.0f) +
+                       w7 * safeSample(samples, i7, 0.0f) +
+                       w8 * safeSample(samples, i8, 0.0f);
 
         return result;
     }
 
-    float safeSample(const std::vector<float>& samples, const int x, const int y, const int z, const float d) {
-        return safeSample(samples, index(x,y,z), d);
+    float safeSample(const std::vector<float> &samples, const int x, const int y, const int z, const float d) {
+        return safeSample(samples, index(x, y, z), d);
     }
 
-    float safeSample(const std::vector<float>& samples, const int i, const float d) {
-        if(i < 0 || i >= RES_X*RES_Y*RES_Z) {
+    float safeSample(const std::vector<float> &samples, const int i, const float d) {
+        if (i < 0 || i >= RES_X * RES_Y * RES_Z) {
             return d;
         }
 
-        int x,y,z; inverseIndex(i, x,y,z);
-        if(x == 0 || y == 0 || z == 0 || x == RES_X - 1 || y == RES_Y - 1 || z == RES_Z - 1) {
+        int x, y, z;
+        inverseIndex(i, x, y, z);
+        if (x == 0 || y == 0 || z == 0 || x == RES_X - 1 || y == RES_Y - 1 || z == RES_Z - 1) {
             return d;
         }
 
         const float v = samples[i];
-        if(std::isnan(v) || std::isinf(v)) {
+        if (std::isnan(v) || std::isinf(v)) {
             return d;
         }
         return v;
     }
-
 };
-
 
 
 int main() {
@@ -588,9 +569,9 @@ int main() {
     FluidSim sim;
     sim.create_sphere_density();
 
-    Line l(Eigen::Vector3f(0.0f, 0.0f, 0.0f), Eigen::Vector3f(1.0f, 0.0f,0.0f));
-    Line l1(Eigen::Vector3f(1.0f, 0.0f, 0.0f), Eigen::Vector3f(.6f, 0.2f,0.0f));
-    Line l2(Eigen::Vector3f(1.0f, 0.0f, 0.0f), Eigen::Vector3f(.6f, -0.2f,0.0f));
+    Line l(Eigen::Vector3f(0.0f, 0.0f, 0.0f), Eigen::Vector3f(1.0f, 0.0f, 0.0f));
+    Line l1(Eigen::Vector3f(1.0f, 0.0f, 0.0f), Eigen::Vector3f(.6f, 0.2f, 0.0f));
+    Line l2(Eigen::Vector3f(1.0f, 0.0f, 0.0f), Eigen::Vector3f(.6f, -0.2f, 0.0f));
 
     double deltaTime = 0.0;
 
@@ -619,7 +600,7 @@ int main() {
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-        //sim.draw_velocity_field(l, l1, l2);
+        // sim.draw_velocity_field(l, l1, l2);
 
         auto endTime = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double, std::milli> elapsed = endTime - startTime;
