e1baa1e3521a72efd2edbe8b4f5a3837d8e39998
nilsegger <nilsxegger@gmail.com>
2024-11-15 16:54:41 +0100

    implemented better gauss-seidel exit

--- advection-reflection/main.cpp
+++ advection-reflection/main.cpp
@@ -31,13 +31,12 @@ public:
                 m_s[i] = CellType::Fluid;
             }
 
-            int x,y,z;
-            inverseIndex(i ,x, y, z);
+            int x, y, z;
+            inverseIndex(i, x, y, z);
 
-            if(index(x,y,z) != i) {
+            if (index(x, y, z) != i) {
                 std::cerr << "index error" << std::endl;
             }
-
         }
     }
 
@@ -55,6 +54,7 @@ public:
     }
 
     void sphere_influence(float t_step) {
+        return;
         iterate([this, t_step](const int i, const int x, const int y, const int z) {
             float x1 = RES_X / 2.0f;
             float y1 = RES_Y / 8.0f;
@@ -69,23 +69,17 @@ public:
 
     void bottom_influence() {
         iterate([this](const int i, const int x, const int y, const int z) {
-            if (y == 1) {
-                m_vv[i] = 0.5f;
-                m_s[i] = CellType::StaticFluid;
-            }
-
-            if(y == RES_Y - 2) {
-                m_vv[i] = -0.5f;
-                m_s[i] = CellType::StaticFluid;
-            }
+            m_vu[i] = (1.0f - (static_cast<float>(x) / static_cast<float>(RES_X))) * 1.0f;
+            m_vv[i] = (1.0f - (static_cast<float>(y) / static_cast<float>(RES_Y))) * 1.0f;
+            m_vw[i] = (1.0f - (static_cast<float>(z) / static_cast<float>(RES_Z))) * 1.0f;
         });
     }
 
     void clear() {
         iterate([this](const int i, const int x, const int y, const int z) {
-           m_vu[i] = 0.0f;
-           m_vv[i] = 0.0f;
-           m_vw[i] = 0.0f;
+            m_vu[i] = 0.0f;
+            m_vv[i] = 0.0f;
+            m_vw[i] = 0.0f;
             m_density[i] = 0.0f;
         });
     }
@@ -100,8 +94,7 @@ public:
 
 
         iterate([t_step, this](const int i, const int x, const int y, const int z) {
-
-            if(m_s[i] == CellType::Fluid) {
+            if (m_s[i] == CellType::Fluid) {
                 //m_vv[i] -= t_step * 9.81; // m_vv[i] //+ (t_step * -9.81f);
             }
 
@@ -111,11 +104,17 @@ public:
 
 
         // 2. make fluid incompressable (projection)
-        for (int i = 0; i < 10; i++) {
-             gauss_seidel_projection();
+        float change = gauss_seidel_projection();
+        int iter = 1;
+        while(change > 1.0f || iter > 1000) {
+            iter++;
+            change = gauss_seidel_projection();
         }
-
-        std::cerr << "Average Divergence: " << avgDivergence() / (RES_X * RES_Y * RES_Z) << '\n';
+        if(iter > 10 && iter < 1000) {
+            std::cerr << "Required " << iter << " to converge to < 1.0f change." << std::endl;
+        } else if(iter == 1000) {
+            std::cerr << "Failed to converge gauss seidel within 1000 iterations. Remaining change " << change << std::endl;
+        }
 
         semi_lagrange(t_step);
     }
@@ -133,10 +132,12 @@ public:
             max_norm = std::max(norm, max_norm);
         });
 
+        max_norm = 1.0f;
+
         iterate([this, &l, &l1, &l2, &l3, &l4, &depth, &max_norm](const int i, const int x, const int y, const int z) {
             if (z != depth) return;
 
-            float max_w = 0.8f / RES_X;
+            float max_w = 1.0f / RES_X;
 
             float u = (m_vu[i] + m_vu[index(x + 1, y, z)]) / 2.0f;
             float v = (m_vv[i] + m_vv[index(x, y + 1, z)]) / 2.0f;
@@ -162,7 +163,9 @@ public:
                 Eigen::Affine>::Identity();
 
             transform_y.translate(Eigen::Vector3f(uv_x, uv_y, uv_z));
-            transform_y.rotate(rotation);
+            if(direction.x() != 1.0f) {
+                transform_y.rotate(rotation);
+            }
             transform_y.scale(max_w / max_norm * norm);
 
             //float sy = std::copysign(std::min(std::abs(m_vv[i] * max_h), max_h), m_vv[i]);
@@ -175,7 +178,12 @@ public:
         });
     }
 
-    void gauss_seidel_projection() {
+    float gauss_seidel_projection() {
+
+        const std::vector ss_vv(m_vv);
+        const std::vector ss_vu(m_vu);
+        const std::vector ss_vw(m_vw);
+
         iterate([this](const int i, const int x, const int y, const int z) {
             if (m_s[i] == CellType::Object) return;
 
@@ -187,14 +195,13 @@ public:
             int izmm = index(x, y, z - 1);
 
             // Gauss-Seidel method
-
             float s = 0.0f;
-            if(m_s[ixpp] == CellType::Fluid) s++;
-            if(m_s[ixmm] == CellType::Fluid) s++;
-            if(m_s[iypp] == CellType::Fluid) s++;
-            if(m_s[iymm] == CellType::Fluid) s++;
-            if(m_s[izpp] == CellType::Fluid) s++;
-            if(m_s[izmm] == CellType::Fluid) s++;
+            if (m_s[ixpp] == CellType::Fluid) s++;
+            if (m_s[ixmm] == CellType::Fluid) s++;
+            if (m_s[iypp] == CellType::Fluid) s++;
+            if (m_s[iymm] == CellType::Fluid) s++;
+            if (m_s[izpp] == CellType::Fluid) s++;
+            if (m_s[izmm] == CellType::Fluid) s++;
 
             float divergence = (m_vu[ixpp] - m_vu[i]) / DX +
                                (m_vv[iypp] - m_vv[i]) / DY +
@@ -208,23 +215,48 @@ public:
             if (m_s[izmm] == CellType::Fluid && m_s[i] == CellType::Fluid) m_vw[i] += dd;
             if (m_s[izpp] == CellType::Fluid) m_vw[izpp] -= dd;
         });
+
+        float change = 0.0f;
+        iterate([this, &change, &ss_vu, &ss_vv, &ss_vw](const int i, const int x, const int y, const int z) {
+            change += (ss_vu[i] - m_vu[i]);
+            change += (ss_vv[i] - m_vv[i]);
+            change += (ss_vw[i] - m_vw[i]);
+        });
+        return change;
     }
 
     void semi_lagrange(const float t_step) {
-        std::vector ss_vv(m_vv);
-        std::vector ss_vu(m_vu);
-        std::vector ss_vw(m_vw);
-        std::vector ss_d(m_density);
+        const std::vector ss_vv(m_vv);
+        const std::vector ss_vu(m_vu);
+        const std::vector ss_vw(m_vw);
+        const std::vector ss_d(m_density);
 
         iterate([this, &ss_vv, &ss_vw, &ss_vu, &ss_d, &t_step](const int i, const int x, const int y, const int z) {
+            if (m_s[i] == Fluid) {
 
-            m_vu[i] = sample(ss_vu, ss_vv, ss_vw, i, x, y, z, SampleDirection::X, t_step);
-            m_vv[i] = sample(ss_vu, ss_vv, ss_vw, i, x, y, z, SampleDirection::Y, t_step);
-            m_vw[i] = sample(ss_vu, ss_vv, ss_vw, i, x, y, z, SampleDirection::Z, t_step);
 
-            // Same backtracking based on the same velocities
-            //m_density[i] = sample_density(ss_d, ss_vu, ss_vv, ss_vw, x, y, z, t_step);
+                float dx = ss_vu[i];
+                float dy = (ss_vv[i] + ss_vv[index(x - 1, y, z)] + ss_vv[index(x, y + 1, z)] + ss_vv[index(
+                                      x - 1, y + 1, z)]) / 4.0f;
+                float dz = (ss_vw[i] + ss_vw[index(x - 1, y, z)] + ss_vw[index(x, y, z + 1)] + ss_vw[index(
+                                      x - 1, y, z + 1)]) / 4.0f;
+                m_vu[i] = sample(x, y, z, dx, dy, dz, t_step, ss_vu);
 
+
+                dx = (ss_vu[i] + ss_vu[index(x, y - 1, z)] + ss_vu[index(x + 1, y, z)] + ss_vu[index(
+                                      x + 1, y - 1, z)]) / 4.0f;
+                dy = ss_vv[i];
+                dz = (ss_vw[i] + ss_vw[index(x, y - 1, z)] + ss_vw[index(x, y, z + 1)] + ss_vw[index(
+                                      x, y - 1, z + 1)]) / 4.0f;
+                m_vv[i] = sample(x, y, z, dx, dy, dz, t_step, ss_vv);
+
+                dx = (ss_vu[i] + ss_vu[index(x, y, z - 1)] + ss_vu[index(x + 1, y, z)] + ss_vu[index(
+                                      x + 1, y, z - 1)]) / 4.0f;
+                dy = (ss_vv[i] + ss_vv[index(x, y, z - 1)] + ss_vv[index(x, y + 1, z)] + ss_vv[index(
+                                                      x, y + 1, z - 1)]) / 4.0f;
+                dz = ss_vw[i];
+                m_vw[i] = sample(x, y, z, dx, dy, dz, t_step, ss_vw);
+            }
         });
     }
 
@@ -232,7 +264,8 @@ public:
         float div = 0.0f;
         float count = 0.0f;
         iterate([this, &div, &count](const int i, const int x, const int y, const int z) {
-            if(m_s[i] == CellType::Fluid) { // Only count non static fields
+            if (m_s[i] == CellType::Fluid) {
+                // Only count non static fields
                 int ixpp = index(x + 1, y, z);
                 int iypp = index(x, y + 1, z);
                 int izpp = index(x, y, z + 1);
@@ -247,7 +280,6 @@ public:
     }
 
 private:
-
     enum CellType {
         Fluid,
         Object,
@@ -268,16 +300,18 @@ private:
 
     float m_advection_speed = 1.0f;
 
-
-
     // staggered grid index
-    static int index(int x, int y, int z) {
+    static int index(const int x, const int y, const int z) {
+        if (x < 0 || y < 0 || z < 0 || x >= RES_X || y >= RES_Y || z >= RES_Z) {
+            return -1;
+        }
+
         return (z * RES_X * RES_Y) + (y * RES_X) + x;
     }
 
     static void inverseIndex(const int index, int &x, int &y, int &z) {
         z = index / (RES_X * RES_Y);
-        const int z_indexed = index - (z*RES_X*RES_Y);
+        const int z_indexed = index - (z * RES_X * RES_Y);
         y = z_indexed / RES_X;
         x = z_indexed % RES_X;
     }
@@ -296,62 +330,40 @@ private:
         X, Y, Z
     };
 
-    // Move back by new velocity and uniformly sample value
-    float sample(std::vector<float> &ss_vu, std::vector<float> &ss_vv, std::vector<float> &ss_vw, const int i,
-                 const int x, const int y, const int z,
-                 SampleDirection direction, float t_step) {
-        // Vector to move back by, different depending on sample direction
-        float vx = safeSample(ss_vu, i, 0.0f) * t_step;
-        float vy = safeSample(ss_vv, i, 0.0f) * t_step;
-        float vz = safeSample(ss_vw, i, 0.0f) * t_step;
+    float sample(const float x, const float y, const float z, const float dx, const float dy, const float dz,
+                 const float t_step, const std::vector<float> &samples) {
+        // Where did particle come from?
+        const float nx = static_cast<float>(x) - dx * t_step;
+        const float ny = static_cast<float>(y) - dy * t_step;
+        const float nz = static_cast<float>(z) - dz * t_step;
 
-        switch (direction) {
-            case X: {
-                vy = (safeSample(ss_vv, i, 0.0f) + safeSample(ss_vv, x - 1, y, z, 0.0f) +
-                      safeSample(ss_vv, x, y + 1, z, 0.0f) + safeSample(ss_vv, x - 1, y + 1, z, 0.0f))
-                     / 4.0f;
-                vz = (safeSample(ss_vw, i, 0.0f) + safeSample(ss_vw, x - 1, y, z, 0.0f) +
-                      safeSample(ss_vw, x, y, z + 1, 0.0f) + safeSample(ss_vw, x - 1, y, z + 1, 0.0f))
-                     / 4.0f;
-                break;
-            }
-            case Y: {
-                vx = (safeSample(ss_vu, i, 0.0f) + safeSample(ss_vu, x + 1, y, z, 0.0f) +
-                      safeSample(ss_vu, x, y - 1, z, 0.0f) + safeSample(ss_vu, x + 1, y - 1, z, 0.0f))
-                     / 4.0f;
-                vz = (safeSample(ss_vw, i, 0.0f) + safeSample(ss_vw, x, y, z + 1, 0.0f) +
-                      safeSample(ss_vw, x, y - 1, z, 0.0f) + safeSample(ss_vw, x, y - 1, z + 1, 0.0f))
-                     / 4.0f;
-                break;
-            }
-            case Z: {
-                vx = (safeSample(ss_vu, i, 0.0f) + safeSample(ss_vu, x + 1, y, z, 0.0f) +
-                      safeSample(ss_vu, x, y, z - 1, 0.0f) + safeSample(ss_vu, x + 1, y, z - 1, 0.0f))
-                     / 4.0f;
-                vy = (safeSample(ss_vv, i, 0.0f) + safeSample(ss_vv, x, y, z - 1, 0.0f) +
-                      safeSample(ss_vv, x, y + 1, z, 0.0f) + safeSample(ss_vv, x, y + 1, z - 1, 0.0f))
-                     / 4.0f;
-                break;
-            }
-        }
+        const float bx = std::floor(nx);
+        const float tx = bx + 1;
+        const float u = (nx - bx) / (tx - bx);
 
-        // Move back
-        float nu = static_cast<float>(x) - vx;
-        float nv = static_cast<float>(x) - vy;
-        float nw = static_cast<float>(x) - vz;
+        const float by = std::floor(ny);
+        const float ty = by + 1;
+        const float v = (ny - by) / (ty - by);
 
-        switch (direction) {
-            case X: return interpolate(nu, nv, nw, ss_vu);
-            case Y: return interpolate(nu, nv, nw, ss_vv);
-            case Z: return interpolate(nu, nv, nw, ss_vw);
-        }
+        const float bz = std::floor(nz);
+        const float tz = bz + 1;
+        const float w = (nz - bz) / (tz - bz);
 
-        return 0.0f;
+        float result = 0.0f;
+        result += safeSample(samples, bx, by, bz, 0.0f) * ((1.0f - u) * (1.0f - v) * (1.0f - w));
+        result += safeSample(samples, tx, by, bz, 0.0f) * ((u) * (1.0f - v) * (1.0f - w));
+        result += safeSample(samples, bx, ty, bz, 0.0f) * ((1.0f - u) * (v) * (1.0f - w));
+        result += safeSample(samples, bx, by, tz, 0.0f) * ((1.0f - u) * (1.0f - v) * (w));
+        result += safeSample(samples, tx, ty, bz, 0.0f) * ((u) * (v) * (1.0f - w));
+        result += safeSample(samples, tx, by, tz, 0.0f) * ((u) * (1.0f - v) * (w));
+        result += safeSample(samples, bx, ty, tz, 0.0f) * ((1.0f - u) * (v) * (w));
+        result += safeSample(samples, tx, ty, tz, 0.0f) * ((u) * (v) * (w));
+        return result;
     }
 
     // Move back by new velocity and uniformly sample value
     float sample_density(std::vector<float> &density, std::vector<float> &ss_vu, std::vector<float> &ss_vv,
-                         std::vector<float> &ss_vw, int x, int y, int z, float t_step) {
+                         std::vector<float> &ss_vw, const int x, const int y, const int z, const float t_step) {
         int i = index(x, y, z);
 
         // Vector to move back by, different depending on sample direction
@@ -361,56 +373,18 @@ private:
 
 
         // Move back, lands within 4 cells origins -> interpolate
-        float nx = static_cast<float>(x) + vx;
-        float ny = static_cast<float>(y) + vy;
-        float nz = static_cast<float>(z) + vz;
+        float nx = static_cast<float>(x) - vx;
+        float ny = static_cast<float>(y) - vy;
+        float nz = static_cast<float>(z) - vz;
 
-        return interpolate(nx, ny, nz, density);
-    }
-
-
-    float interpolate(const float nx, const float ny, const float nz, const std::vector<float> &samples) {
-        float x1 = std::floor(nx);
-        float x2 = x1 + 1;
-        float y1 = std::floor(ny);
-        float y2 = y1 + 1;
-        float z1 = std::floor(nz);
-        float z2 = z1 + 1;
-
-        float u = (nx - x1) / (x2 - x1);
-        float v = (ny - y1) / (y2 - y1);
-        float w = (nz - z1) / (z2 - z1);
-
-        int i1 = index(x1, y1, z1);
-        float w1 = (1.0f - u) * (1.0f - v) * (1.0f - w);
-        int i2 = index(x2, y1, z1);
-        float w2 = u * (1.0f - v) * (1.0f - w);
-        int i3 = index(x1, y2, z1);
-        float w3 = (1.0f - u) * v * (1.0f - w);
-        int i4 = index(x1, y1, z2);
-        float w4 = (1.0f - u) * (1.0f - v) * w;
-        int i5 = index(x2, y2, z1);
-        float w5 = u * v * (1.0f - w);
-        int i6 = index(x2, y1, z2);
-        float w6 = u * (1.0f - v) * w;
-        int i7 = index(x1, y2, z2);
-        float w7 = (1.0f - u) * v * w;
-        int i8 = index(x2, y2, z2);
-        float w8 = u * v * w;
-
-        float result = w1 * safeSample(samples, i1, 0.0f) +
-                       w2 * safeSample(samples, i2, 0.0f) +
-                       w3 * safeSample(samples, i3, 0.0f) +
-                       w4 * safeSample(samples, i4, 0.0f) +
-                       w5 * safeSample(samples, i5, 0.0f) +
-                       w6 * safeSample(samples, i6, 0.0f) +
-                       w7 * safeSample(samples, i7, 0.0f) +
-                       w8 * safeSample(samples, i8, 0.0f);
-
-        return result;
+        return 0.0f;
     }
 
     float safeSample(const std::vector<float> &samples, const int x, const int y, const int z, const float d) {
+        if (x < 0 || y < 0 || z < 0 || x >= RES_X || y >= RES_Y || z >= RES_Z) {
+            return d;
+        }
+
         return safeSample(samples, index(x, y, z), d);
     }
 
@@ -426,10 +400,10 @@ private:
         }
 
         const float v = samples[i];
-        if (std::isnan(v) || std::isinf(v)) {
-            return d;
+        if (std::isfinite(v)) {
+            return v;
         }
-        return v;
+        return d;
     }
 
     bool isBoundary(const int i) {
@@ -448,7 +422,9 @@ int main() {
         return -1;
     };
 
-    std::cout << "Controls\nSpace: Switch between density / fluid.\nS: Single step simulation.\nP: Play simulation.\n1: Projection\n2: Semi-Lagrange" <<
+    std::cout <<
+            "Controls\nSpace: Switch between density / fluid.\nS: Single step simulation.\nP: Play simulation.\n1: Projection\n2: Semi-Lagrange"
+            <<
             std::endl;
 
     FluidSim sim;
@@ -478,7 +454,7 @@ int main() {
         if (renderer.upPressed()) fieldRenderDepth++;
         if (renderer.downPressed()) fieldRenderDepth--;
 
-        if(renderer.rPressed()) {
+        if (renderer.rPressed()) {
             sim.clear();
             sim.bottom_influence();
         }
@@ -497,12 +473,13 @@ int main() {
             std::cout << "Frame time: " << deltaTime << " ms." << std::endl;
         }
 
-        if(renderer.Pressed1()) {
-            sim.gauss_seidel_projection();
+        if (renderer.Pressed1()) {
+            float change = sim.gauss_seidel_projection();
+            std::cout << "Changed: " << change << std::endl;
         }
 
-        if(renderer.Pressed2()) {
-            sim.semi_lagrange(1.0 / 30.0);
+        if (renderer.Pressed2()) {
+            sim.semi_lagrange(1.0f / 30.0f);
         }
 
         renderer.clear_frame();
